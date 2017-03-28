1794520962dc8bcc2f90a0394349be0d7bfb0534
Tzeho Tong <tangzihao@pku.edu.cn>
2017-03-28 13:01:57 +0800

    Update and rename xgboost_train.py to xgboost_.py

--- xgboost_.py
+++ xgboost_.py
@@ -1,9 +1,10 @@
 import numpy as np
 import xgboost as xgb
-from data import *
+from fetcher import *
 # label need to be 0 to num_class-1
-champion_num = 134
-train_X, train_Y, test_X, test_Y = read_file_both_sides("AramDataSet624.txt", "ChampionList624.txt", champion_num)
+champion_dict = fetch_champion_dict("champion.json")
+champion_num = len(champion_dict)
+X_train, y_train, X_test, y_test = fetch_both_sides("arurf", champion_dict)
 xg_train = xgb.DMatrix(train_X, label=train_Y)
 xg_test = xgb.DMatrix(test_X, label=test_Y)
 # setup parameters for xgboost
@@ -19,9 +20,9 @@ param['alpha'] = 1
 param['objective'] = 'multi:softmax'
 param['num_class'] = 2
 watchlist = [ (xg_train,'train'), (xg_test, 'test') ]
-num_round = 500
+num_round = 50
 bst = xgb.train(param, xg_train, num_round, watchlist )
 # get prediction
 pred = bst.predict( xg_test )
 print ('predicting, classification error=%f' % (sum( int(pred[i]) != test_Y[i] for i in range(len(test_Y))) / float(len(test_Y)) ))
-bst.save_model('aram_xgboost.model')
+#st.save_model('aram_xgboost.model')
