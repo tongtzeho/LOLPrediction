497cb6dd33128b8dc1521e3f6694722c27abe77d
tongtzeho <tangzihao@pku.edu.cn>
2017-04-04 18:34:56 +0800

    Update fetcher.py

--- fetcher.py
+++ fetcher.py
@@ -14,7 +14,7 @@ def fetch_champion_dict(jsonfile):
 		champion_dict = json.load(jsonfile)
 	return champion_dict
 		
-def fetch_both_sides_tgp(gamemode, champion_dict, train_set_rate=0.75):
+def fetch_both_sides_tgp(gamemode, champion_dict):
 	X_train_lst = []
 	y_train_lst = []
 	X_test_lst = []
@@ -39,6 +39,7 @@ def fetch_both_sides_tgp(gamemode, champion_dict, train_set_rate=0.75):
 	except:
 		print ("Exception: Selecting")
 		return (X_train, y_train, X_test, y_test)	
+	cnt = 0
 	for line in all_set:
 		X = np.zeros(len(champion_dict)*2)
 		y = line[3]
@@ -57,7 +58,8 @@ def fetch_both_sides_tgp(gamemode, champion_dict, train_set_rate=0.75):
 			else :
 				X[curchampionindex+len(champion_dict)] = 1
 		if valid:
-			if (random.random() < train_set_rate) :
+			cnt += 1
+			if (cnt % 4 != 0) :
 				X_train_lst.append(X)
 				y_train_lst.append(y)
 			else :
@@ -70,7 +72,7 @@ def fetch_both_sides_tgp(gamemode, champion_dict, train_set_rate=0.75):
 	print ('Success: Fetch Both Sides')
 	return (X_train, y_train, X_test, y_test)
 
-def fetch_one_side_tgp(gamemode, champion_dict, train_set_rate=0.75):
+def fetch_one_side_tgp(gamemode, champion_dict):
 	X_train_lst = []
 	y_train_lst = []
 	X_test_lst = []
@@ -94,7 +96,8 @@ def fetch_one_side_tgp(gamemode, champion_dict, train_set_rate=0.75):
 			conn.close()
 	except:
 		print ("Exception: Selecting")
-		return (X_train, y_train, X_test, y_test)	
+		return (X_train, y_train, X_test, y_test)
+	cnt = 0
 	for line in all_set:
 		X = np.zeros(len(champion_dict))
 		y = line[3]
@@ -110,7 +113,8 @@ def fetch_one_side_tgp(gamemode, champion_dict, train_set_rate=0.75):
 				break
 			X[curchampionindex] = 1
 		if valid:
-			if (random.random() < train_set_rate) :
+			cnt += 1
+			if (cnt % 8 != 0 and cnt % 8 != 3) :
 				X_train_lst.append(X)
 				y_train_lst.append(y)
 			else :
@@ -130,7 +134,8 @@ def fetch_one_side_tgp(gamemode, champion_dict, train_set_rate=0.75):
 				break
 			X[curchampionindex] = 1
 		if valid:
-			if (random.random() < train_set_rate) :
+			cnt += 1
+			if (cnt % 8 != 0 and cnt % 8 != 3) :
 				X_train_lst.append(X)
 				y_train_lst.append(y)
 			else :
@@ -143,7 +148,7 @@ def fetch_one_side_tgp(gamemode, champion_dict, train_set_rate=0.75):
 	print ('Success: Fetch One Side')
 	return (X_train, y_train, X_test, y_test)
 	
-def fetch_both_sides_riot(mapid, gametype, subtype, gamemode, createdate, champion_dict, train_set_rate=0.75):
+def fetch_both_sides_riot(mapid, gametype, subtype, gamemode, createdate, champion_dict):
 	X_train_lst = []
 	y_train_lst = []
 	X_test_lst = []
@@ -168,6 +173,7 @@ def fetch_both_sides_riot(mapid, gametype, subtype, gamemode, createdate, champi
 	except:
 		print ("Exception: Selecting")
 		return (X_train, y_train, X_test, y_test)
+	cnt = 0
 	for line in all_set:
 		X = np.zeros(len(champion_dict)*2)
 		y = line[0]
@@ -177,7 +183,8 @@ def fetch_both_sides_riot(mapid, gametype, subtype, gamemode, createdate, champi
 		for championidstr in line[2].split(';'):
 			championid = int(champion_dict[championidstr][0])
 			X[championid+len(champion_dict)] = 1
-		if (random.random() < train_set_rate) :
+		cnt += 1
+		if (cnt % 4 != 0) :
 			X_train_lst.append(X)
 			y_train_lst.append(y)
 		else :
@@ -188,4 +195,61 @@ def fetch_both_sides_riot(mapid, gametype, subtype, gamemode, createdate, champi
 	X_test = np.array(X_test_lst)
 	y_test = np.array(y_test_lst)
 	print ('Success: Fetch Both Sides')
+	return (X_train, y_train, X_test, y_test)
+	
+def fetch_one_side_riot(mapid, gametype, subtype, gamemode, createdate, champion_dict):
+	X_train_lst = []
+	y_train_lst = []
+	X_test_lst = []
+	y_test_lst = []
+	X_train = np.array(X_train_lst)
+	y_train = np.array(y_train_lst)
+	X_test = np.array(X_test_lst)
+	y_test = np.array(y_test_lst)
+	conn = connect_db('lol_prediction_riot')
+	if conn == None: return (X_train, y_train, X_test, y_test)
+	cursor = conn.cursor()
+	try:
+		ifexists = cursor.execute("select bluesidewin, bluesidechampion, redsidechampion from game_riot where mapid="+mapid+" and gametype='"+gametype+"' and subtype='"+subtype+"' and gamemode='"+gamemode+"' and createdate>="+createdate[0]+" and createdate<="+createdate[1])
+		if ifexists == 0:
+			cursor.close()
+			conn.close()
+			return (X_train, y_train, X_test, y_test)
+		else:
+			all_set = cursor.fetchall()
+			cursor.close()
+			conn.close()
+	except:
+		print ("Exception: Selecting")
+		return (X_train, y_train, X_test, y_test)
+	cnt = 0
+	for line in all_set:
+		X = np.zeros(len(champion_dict))
+		y = line[0]
+		for championidstr in line[1].split(';'):
+			championid = int(champion_dict[championidstr][0])
+			X[championid] = 1
+		cnt += 1
+		if (cnt % 8 != 0 and cnt % 8 != 3):
+			X_train_lst.append(X)
+			y_train_lst.append(y)
+		else:
+			X_test_lst.append(X)
+			y_test_lst.append(y)
+		X = np.zeros(len(champion_dict))
+		for championidstr in line[2].split(';'):
+			championid = int(champion_dict[championidstr][0])
+			X[championid] = 1
+		cnt += 1
+		if (cnt % 8 != 0 and cnt % 8 != 3):
+			X_train_lst.append(X)
+			y_train_lst.append(1-y)
+		else:
+			X_test_lst.append(X)
+			y_test_lst.append(1-y)
+	X_train = np.array(X_train_lst)
+	y_train = np.array(y_train_lst)
+	X_test = np.array(X_test_lst)
+	y_test = np.array(y_test_lst)
+	print ('Success: Fetch One Side')
 	return (X_train, y_train, X_test, y_test)
