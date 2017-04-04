6f55b2b6ad0d71e9688d354a67e0ac94b1db86bd
tongtzeho <tangzihao@pku.edu.cn>
2017-04-04 18:38:24 +0800

    Add example for prediction

--- neural_network.py
+++ neural_network.py
@@ -19,10 +19,21 @@ from fetcher import *
 
 batch_size = 256
 nb_classes = 2
-nb_epoch = 30
+nb_epoch = 50
 
 champion_dict = fetch_champion_dict("champion.json")
 champion_num = len(champion_dict)
+
+#X_train, y_train, X_test, y_test = fetch_one_side_tgp('arurf', champion_dict)
+#X_train, y_train, X_test, y_test = fetch_one_side_tgp('rank', champion_dict)
+#X_train, y_train, X_test, y_test = fetch_one_side_riot('11', 'MATCHED_GAME', 'NORMAL', 'CLASSIC', ('1490371200000', '1800371200000'), champion_dict)
+#X_train, y_train, X_test, y_test = fetch_one_side_riot('11', 'MATCHED_GAME', 'RANKED_SOLO_5x5', 'CLASSIC', ('1490371200000', '1800371200000'), champion_dict)
+#X_train, y_train, X_test, y_test = fetch_one_side_riot('12', 'MATCHED_GAME', 'ARAM_UNRANKED_5x5', 'ARAM', ('1490371200000', '1800371200000'), champion_dict)
+
+#X_train, y_train, X_test, y_test = fetch_both_sides_tgp('arurf', champion_dict)
+#X_train, y_train, X_test, y_test = fetch_both_sides_tgp('rank', champion_dict)
+#X_train, y_train, X_test, y_test = fetch_both_sides_riot('11', 'MATCHED_GAME', 'NORMAL', 'CLASSIC', ('1490371200000', '1800371200000'), champion_dict)
+#X_train, y_train, X_test, y_test = fetch_both_sides_riot('11', 'MATCHED_GAME', 'RANKED_SOLO_5x5', 'CLASSIC', ('1490371200000', '1800371200000'), champion_dict)
 X_train, y_train, X_test, y_test = fetch_both_sides_riot('12', 'MATCHED_GAME', 'ARAM_UNRANKED_5x5', 'ARAM', ('1490371200000', '1800371200000'), champion_dict)
 		
 X_train = X_train.astype('int8')
